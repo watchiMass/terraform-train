8bbd7fe47069594dc749bd11c46000ab8db958ee
watchiMass <122853168+watchiMass@users.noreply.github.com>
2026-01-08 21:44:05 +0100

    Update AWS credentials file path in main.tf

--- tp-4/backend/main.tf
+++ tp-4/backend/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region  = "us-east-1"
-  shared_credentials_files = ["/home/jeriel/Téléchargements/devops_projects/terraform-train/.secrets/credentials"]
-  profile = "default"
+shared_credentials_files = ["../.secrets/credentials"] 
+profile = "default"
 }
 
 
